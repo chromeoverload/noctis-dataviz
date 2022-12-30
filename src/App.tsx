b6caf3fc018166b3265222a7144be82381867526
jbutler87 <chromeoverload@gmail.com>
2022-12-30 00:57:56 -0500

    basic project setup; library research

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Test</h1>
       </header>
+      <svg id="Viz_area" height={600} width={1200} />
     </div>
   );
 }
